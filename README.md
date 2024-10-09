01ff89a15c65c6e8ab744c1453acc3f817ea3501
praveenkotipalli <praveenin321@gmail.com>
2024-10-09 14:39:43 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# sushma-s-sdp3" 
---
+++ src/main/java/com/example/Food_app/prog/entity/UserDtls.java
@@ -0,0 +1,65 @@
+package com.example.Food_app.prog.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="USER_DTLS")
+public class UserDtls {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="Name")
+private int id;
+private String fullname;
+private String dob;
+private String gender;
+private String address;
+private String email;
+private String password;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getFullname() {
+	return fullname;
+}
+public void setFullname(String fullname) {
+	this.fullname = fullname;
+}
+public String getDob() {
+	return dob;
+}
+public void setDob(String dob) {
+	this.dob = dob;
+}
+public String getGender() {
+	return gender;
+}
+public void setGender(String gender) {
+	this.gender = gender;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+}
